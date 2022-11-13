ff068497d98a7fae76b312c53b792d508de796a0
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-13 14:09:11 +0700

    Update homepage.py

--- main/assistant/modules/callbacks/homepage.py
+++ main/assistant/modules/callbacks/homepage.py
@@ -1,21 +1,3 @@
-""".
-This file creates home page of helpmenu.
-"""
-
-from pyrogram import filters
-
-from pyrogram.types import (
-    InlineKeyboardMarkup,
-    InputMediaPhoto,
-    CallbackQuery,
-)
-
-from main.userbot.client import app
-
-
-
-
-
 @app.bot.on_callback_query(filters.regex("home-tab"))
 @app.alert_user
 async def _start(_, cb: CallbackQuery):
@@ -24,17 +6,15 @@ async def _start(_, cb: CallbackQuery):
         reply_markup=InlineKeyboardMarkup([
                 app.BuildKeyboard(
                     (
-                        ["• Settings •", "settings-tab"],
                         ["• Plugins •", "plugins-tab"]
                     )
                 ),
                 app.BuildKeyboard(
                     (
-                        ["• Extra •", "extra-tab"],
-                        ["• Stats •", "stats-tab"]
+                        ["• Diskusi •", "https://t.me/ruangdiskusikami"],
+                        ["• Update •", "https://t.me/ruangprojects"]
                     )
                 ),
-                app.BuildKeyboard(([["About", "about-tab"]])),
                 app.BuildKeyboard(([["Close", "close-tab"]]))
         ]
         ),
