c0acfdabffe164f69cc2b121eeb26b4431335c7b
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-13 14:53:13 +0700

    Update homepage.py

--- main/assistant/modules/callbacks/homepage.py
+++ main/assistant/modules/callbacks/homepage.py
@@ -20,17 +20,7 @@ async def _start(_, cb: CallbackQuery):
     await cb.edit_message_media(
         media=InputMediaPhoto(media=app.BotPic(), caption=app.home_tab_string()),
         reply_markup=InlineKeyboardMarkup([
-                app.BuildKeyboard(
-                    (
-                        ["• Plugins •", "plugins-tab"]
-                    )
-                ),
-                app.BuildKeyboard(
-                    (
-                        ["• Diskusi •", "https://t.me/ruangdiskusikami"],
-                        ["• Update •", "https://t.me/ruangprojects"]
-                    )
-                ),
+                app.BuildKeyboard(([["Plugins", "plugins-tab"]])),
                 app.BuildKeyboard(([["Close", "close-tab"]]))
         ]
         ),
