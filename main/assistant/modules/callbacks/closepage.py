90c019cd25fc438eaadda53829954e8cc40dac71
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-14 23:42:11 +0700

    Update closepage.py

--- main/assistant/modules/callbacks/closepage.py
+++ main/assistant/modules/callbacks/closepage.py
@@ -36,3 +36,33 @@ async def _close(_, cb: CallbackQuery):
             ]
         )
     )
+
+@app.bot.on_callback_query(filters.regex("support"))
+@app.alert_user
+async def _close(_, cb: CallbackQuery):
+    await cb.edit_message_media(
+        media=InputMediaPhoto(
+            media="main/core/resources/images/pyroby.jpg", 
+            caption=app.close_tab_string()
+        ),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Group Support",
+                        callback_data="https:/t.me/ruangdiskusikami"
+                    ),
+                    InlineKeyboardButton(
+                        text="Channel Update",
+                        callback_data="https:/t.me/ruangprojects"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Open",
+                        callback_data="home-tab"
+                    )
+                ]
+            ]
+        )
+    )
