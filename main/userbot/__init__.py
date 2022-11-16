1f784271b95e682fc19c2a670b9538a6b56c4d08
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-16 10:19:24 +0700

    Update __init__.py

--- main/userbot/__init__.py
+++ main/userbot/__init__.py
@@ -2,4 +2,4 @@ from .client import app
 from pyrogram import Client as Bot
 from config import API_ID, API_HASH, BOT_TOKEN, SESSION
 
-client = Bot(STRING_SESSION, API_ID, API_HASH, plugins=dict(root="main.userbot.modules"))
+client = Bot(STRING_SESSION, API_ID, API_HASH, plugins=dict(root="main.userbot.modules.plugins"))
